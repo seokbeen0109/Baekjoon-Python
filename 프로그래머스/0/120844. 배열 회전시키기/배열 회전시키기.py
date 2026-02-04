db0077c94f18bae244fab1421d972827493998a0
seokbeen0109 <blackwiz09@gmail.com>
2026-02-04 12:45:31 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,12 @@
+def solution(numbers, direction):
+    answer = []
+    if direction == 'left':
+        for i in range(1,len(numbers)):
+            answer.append(numbers[i])
+        answer.append(numbers[0])
+    else:
+        answer.append(numbers[-1])
+        for i in range(0,len(numbers)-1):
+            answer.append(numbers[i])
+        
+    return answer
