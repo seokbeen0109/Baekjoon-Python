d10d883b5d6e54df2cbcd539df4a202f7bac64eb
seokbeen0109 <blackwiz09@gmail.com>
2026-09-09 22:13:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,6 @@
+def solution(s):
+    answer = 0
+    num={'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+    for word, digit in num.items():
+        s=s.replace(word,digit)
+    return int(s)
