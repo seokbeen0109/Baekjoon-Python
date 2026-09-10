6c46b5acd5bbccab486c6c79ae3f38d21dab4d07
seokbeen0109 <blackwiz09@gmail.com>
2026-09-10 21:25:00 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, r.CREATED_DATE
+from USED_GOODS_BOARD b
+join USED_GOODS_REPLY r on b.BOARD_ID=r.BOARD_ID
+where b.created_date like '2022-10%'
+order by r.created_date, b.title
