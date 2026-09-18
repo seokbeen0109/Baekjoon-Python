a912b7f55faf00378d27b3ea92cda6a4baac35b2
seokbeen0109 <blackwiz09@gmail.com>
2026-09-18 22:26:16 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,6 @@
+SELECT i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, round(avg(r.REVIEW_SCORE),2) as SCORE
+from REST_INFO i
+join REST_REVIEW r on i.REST_ID=r.REST_ID
+where i.address like '서울%'
+group by i.REST_ID
+order by score desc, i.favorites desc
