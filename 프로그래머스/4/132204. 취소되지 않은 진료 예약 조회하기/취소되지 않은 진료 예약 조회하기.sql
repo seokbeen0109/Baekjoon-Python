be08abbae5e0d6a6c3a5e11a5ce87ee3d966c7d3
seokbeen0109 <blackwiz09@gmail.com>
2026-09-18 21:58:01 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, d.MCDP_CD, d.DR_NAME, a.APNT_YMD
+from PATIENT p
+join APPOINTMENT a on p.PT_NO=a.PT_NO
+join DOCTOR d on d.DR_ID=a.MDDR_ID
+where a.APNT_YMD like '2022-04-13%' and a.MCDP_CD='CS' and a.APNT_CNCL_YN='N'
+order by a.APNT_YMD
